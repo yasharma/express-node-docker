309bf4b314753f99d6642ebabdea59e0c114b4cc
yash sharma <yash_sharma@seologistics.com>
2018-04-20 13:23:20 +0530

    Update config.yml

--- .dockerbuild.sh
+++ .dockerbuild.sh
@@ -3,5 +3,8 @@
 # Build a docker image
 docker build -t dockerapp .
 
+# Build for production
+docker build -t dockerapp-production -f Dockerfile-production .
+
 # Run the docker image with volume
 docker run -p 3001:3001 -v /home/yash/dockerapp/:/usr/src/app -it --name running-docker-app-with-volume dockerapp
